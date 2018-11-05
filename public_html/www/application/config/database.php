4150ad61801b08f8b65c5410d82f1b7beed48929
김성식 <g1915@passntrip.com>
2018-11-06 04:08:32 +0900

    fff

--- public_html/www/application/config/database.php
+++ public_html/www/application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'admin';
+$db['default']['username'] = 'root';
 $db['default']['password'] = 'music7273!';
 $db['default']['database'] = 'mroo';
 $db['default']['dbdriver'] = 'mysqli';
